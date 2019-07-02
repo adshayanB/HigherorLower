334af0eb642025217003a682bfe3246eec44c40c
adshayanB <adshayan.b@gmail.com>
2019-07-02 15:21:25 -0400

    Initial commit

---
+++ app/src/main/java/ads/higherorlower/MainActivity.java
@@ -0,0 +1,53 @@
+package ads.higherorlower;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    //Variable Scoping
+    int randomNumber;
+    public void makeToast (String string ){
+
+        Toast.makeText(MainActivity.this,string, Toast.LENGTH_SHORT).show();
+
+    }
+
+   public void guess(View view){
+       EditText guessVal = findViewById(R.id.guess);
+
+       int guessInt = Integer.parseInt(guessVal.getText().toString());
+
+
+        if (guessInt > randomNumber){
+
+            makeToast ("Lower!");
+        }
+
+        else if (guessInt < randomNumber){
+
+          makeToast ("Higher!");
+        }
+        else{
+            makeToast("That's Right! Try again!");
+            Random rand = new Random();
+            randomNumber = rand.nextInt( 20)+1;
+        }
+
+
+   }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//Generates Random number when app is launched
+        Random rand = new Random();
+
+        randomNumber = rand.nextInt(20) + 1;
+    }
+}
